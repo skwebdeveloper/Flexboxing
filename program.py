31222696d97eca2750b69b70115902f54df60f80
skwebdeveloper <43346025+skwebdeveloper@users.noreply.github.com>
2020-01-19 18:22:30 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,19 @@
+#Program 1
+principal = 7800
+rate of interest = 7.7
+time = 26
+interest =(principal*rate of interest*time)/100
+print(interest)
+
+#Program 2
+input number
+while(number!=0)do
+Temp = number
+if(Temp == reverse)then
+number = number/10
+remainder = number%10
+reverse = remainder*10 + reverse
+display "Palindrome"
+end-while
+end-if 
+
